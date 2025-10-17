74ca02c3f679ac7404fa2218efd8e647d9ee7611
CaioCampeloop <126394856+CaioCampeloop@users.noreply.github.com>
2025-10-17 16:52:45 -0300

    terminei_1

--- calcular_transformador_El/Form1.Designer.cs
+++ calcular_transformador_El/Form1.Designer.cs
@@ -52,6 +52,28 @@
             this.label11 = new System.Windows.Forms.Label();
             this.textBox_VS = new System.Windows.Forms.TextBox();
             this.textBox_VP = new System.Windows.Forms.TextBox();
+            this.textBox_SA = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox_CP = new System.Windows.Forms.TextBox();
+            this.textBox_FR = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label17 = new System.Windows.Forms.Label();
+            this.textBox_S2 = new System.Windows.Forms.TextBox();
+            this.textBox_PS = new System.Windows.Forms.TextBox();
+            this.textBox_PP = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox_P2 = new System.Windows.Forms.TextBox();
+            this.label19 = new System.Windows.Forms.Label();
+            this.textBox_DC = new System.Windows.Forms.TextBox();
+            this.label20 = new System.Windows.Forms.Label();
+            this.textBox_BP = new System.Windows.Forms.TextBox();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.textBox_BS = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox_G
@@ -136,11 +158,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(323, 165);
+            this.button1.Location = new System.Drawing.Point(124, 279);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 10;
-            this.button1.Text = "button1";
+            this.button1.Text = "Resultado";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -211,7 +233,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(277, 378);
+            this.label10.Location = new System.Drawing.Point(230, 378);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(93, 13);
             this.label10.TabIndex = 22;
@@ -220,7 +242,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(287, 348);
+            this.label11.Location = new System.Drawing.Point(240, 348);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(73, 13);
             this.label11.TabIndex = 21;
@@ -228,23 +250,225 @@
             // 
             // textBox_VS
             // 
-            this.textBox_VS.Location = new System.Drawing.Point(376, 371);
+            this.textBox_VS.Location = new System.Drawing.Point(329, 371);
             this.textBox_VS.Name = "textBox_VS";
             this.textBox_VS.Size = new System.Drawing.Size(100, 20);
             this.textBox_VS.TabIndex = 20;
             // 
             // textBox_VP
             // 
-            this.textBox_VP.Location = new System.Drawing.Point(376, 345);
+            this.textBox_VP.Location = new System.Drawing.Point(329, 345);
             this.textBox_VP.Name = "textBox_VP";
             this.textBox_VP.Size = new System.Drawing.Size(100, 20);
             this.textBox_VP.TabIndex = 19;
+            // 
+            // textBox_SA
+            // 
+            this.textBox_SA.Location = new System.Drawing.Point(686, 224);
+            this.textBox_SA.Name = "textBox_SA";
+            this.textBox_SA.Size = new System.Drawing.Size(100, 20);
+            this.textBox_SA.TabIndex = 23;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(619, 231);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(48, 13);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "Amperes";
+            // 
+            // textBox_CP
+            // 
+            this.textBox_CP.Location = new System.Drawing.Point(686, 370);
+            this.textBox_CP.Name = "textBox_CP";
+            this.textBox_CP.Size = new System.Drawing.Size(100, 20);
+            this.textBox_CP.TabIndex = 25;
+            // 
+            // textBox_FR
+            // 
+            this.textBox_FR.Location = new System.Drawing.Point(686, 58);
+            this.textBox_FR.Name = "textBox_FR";
+            this.textBox_FR.Size = new System.Drawing.Size(100, 20);
+            this.textBox_FR.TabIndex = 29;
+            this.textBox_FR.TextChanged += new System.EventHandler(this.textBox_FR_TextChanged);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(569, 65);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(98, 13);
+            this.label15.TabIndex = 30;
+            this.label15.Text = "fator de rendimento";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(652, 279);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 31;
+            this.button2.Text = "Resultado";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(606, 182);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(61, 13);
+            this.label17.TabIndex = 33;
+            this.label17.Text = "Secundario";
+            // 
+            // textBox_S2
+            // 
+            this.textBox_S2.Location = new System.Drawing.Point(686, 175);
+            this.textBox_S2.Name = "textBox_S2";
+            this.textBox_S2.Size = new System.Drawing.Size(100, 20);
+            this.textBox_S2.TabIndex = 32;
+            // 
+            // textBox_PS
+            // 
+            this.textBox_PS.Location = new System.Drawing.Point(686, 341);
+            this.textBox_PS.Name = "textBox_PS";
+            this.textBox_PS.Size = new System.Drawing.Size(100, 20);
+            this.textBox_PS.TabIndex = 34;
+            // 
+            // textBox_PP
+            // 
+            this.textBox_PP.Location = new System.Drawing.Point(686, 315);
+            this.textBox_PP.Name = "textBox_PP";
+            this.textBox_PP.Size = new System.Drawing.Size(100, 20);
+            this.textBox_PP.TabIndex = 35;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(581, 373);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(100, 13);
+            this.label13.TabIndex = 27;
+            this.label13.Text = "Corrente_Primario A";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(543, 348);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(138, 13);
+            this.label16.TabIndex = 36;
+            this.label16.Text = "Potencia do Secundário VA";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(559, 322);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(121, 13);
+            this.label18.TabIndex = 37;
+            this.label18.Text = "Potencia do Primário VA";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(606, 152);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(44, 13);
+            this.label14.TabIndex = 39;
+            this.label14.Text = "Primario";
+            // 
+            // textBox_P2
+            // 
+            this.textBox_P2.Location = new System.Drawing.Point(686, 145);
+            this.textBox_P2.Name = "textBox_P2";
+            this.textBox_P2.Size = new System.Drawing.Size(100, 20);
+            this.textBox_P2.TabIndex = 38;
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(543, 96);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(124, 13);
+            this.label19.TabIndex = 41;
+            this.label19.Text = "Dencidade de corrente J";
+            // 
+            // textBox_DC
+            // 
+            this.textBox_DC.Location = new System.Drawing.Point(686, 93);
+            this.textBox_DC.Name = "textBox_DC";
+            this.textBox_DC.Size = new System.Drawing.Size(100, 20);
+            this.textBox_DC.TabIndex = 40;
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(543, 113);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(82, 13);
+            this.label20.TabIndex = 42;
+            this.label20.Text = "J = 3,0 A/mm^2";
+            // 
+            // textBox_BP
+            // 
+            this.textBox_BP.Location = new System.Drawing.Point(686, 406);
+            this.textBox_BP.Name = "textBox_BP";
+            this.textBox_BP.Size = new System.Drawing.Size(100, 20);
+            this.textBox_BP.TabIndex = 43;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(580, 413);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(104, 13);
+            this.label21.TabIndex = 44;
+            this.label21.Text = "Bitola Primario mm^2";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(563, 432);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(121, 13);
+            this.label22.TabIndex = 46;
+            this.label22.Text = "Bitola Secundário mm^2";
+            // 
+            // textBox_BS
+            // 
+            this.textBox_BS.Location = new System.Drawing.Point(686, 425);
+            this.textBox_BS.Name = "textBox_BS";
+            this.textBox_BS.Size = new System.Drawing.Size(100, 20);
+            this.textBox_BS.TabIndex = 45;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.textBox_BS);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.textBox_BP);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.textBox_DC);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox_P2);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.textBox_PP);
+            this.Controls.Add(this.textBox_PS);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.textBox_S2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.textBox_FR);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox_CP);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox_SA);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.textBox_VS);
@@ -300,6 +524,28 @@
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.TextBox textBox_VS;
         private System.Windows.Forms.TextBox textBox_VP;
+        private System.Windows.Forms.TextBox textBox_SA;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox_CP;
+        private System.Windows.Forms.TextBox textBox_FR;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.TextBox textBox_S2;
+        private System.Windows.Forms.TextBox textBox_PS;
+        private System.Windows.Forms.TextBox textBox_PP;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox_P2;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.TextBox textBox_DC;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.TextBox textBox_BP;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.TextBox textBox_BS;
     }
 }
 
--- calcular_transformador_El/Form1.cs
+++ calcular_transformador_El/Form1.cs
@@ -12,9 +12,18 @@ namespace calcular_transformador_El
 {
     public partial class Form1 : Form
     {
+        double SecundarioAmperes;
+        double Secundario2;
+        double FatorRendimento;
+        double PotenciaPrimario;
+
+        double densidadeDecorrente_J;
         public Form1()
         {
             InitializeComponent();
+
+
+           
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -36,6 +45,8 @@ namespace calcular_transformador_El
             var Primario = Convert.ToDouble(textBox_P.Text);
             var Secundario = Convert.ToDouble(textBox_S.Text);
 
+            
+
             textBox_RA.Text = AreaTotal.ToString();
 
             var VoltasPorVolt = ConstanteG / AreaTotal;
@@ -51,11 +62,55 @@ namespace calcular_transformador_El
             textBox_VP.Text = VoltasPrimario.ToString();
             textBox_VS.Text = VotasSecundario.ToString();
 
+            
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            textBox_FR.Text = "0,9";
+            textBox_G.Text = "45";
+            textBox_DC.Text = "3";
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FatorRendimento = Convert.ToDouble(textBox_FR.Text);
+
+            Secundario2 = Convert.ToDouble(textBox_S2.Text);
+
+            SecundarioAmperes = Convert.ToDouble(textBox_SA.Text);
+
+            densidadeDecorrente_J = Convert.ToDouble(textBox_DC.Text);
+            
+            var Primario2 = Convert.ToDouble(textBox_P2.Text);
+            
+            var PotenciaSecundario = Secundario2 * SecundarioAmperes;
+            
+            PotenciaPrimario = PotenciaSecundario / FatorRendimento;
+
+            textBox_PP.Text = PotenciaPrimario.ToString();
+            textBox_PS.Text = PotenciaSecundario.ToString();
+
+            var CorrentePrimario = PotenciaPrimario / Primario2;
+
+            textBox_CP.Text = CorrentePrimario.ToString();
+
+            var BitolaPrimario = CorrentePrimario / densidadeDecorrente_J;
+
+            var BitolaSecundario = SecundarioAmperes / densidadeDecorrente_J;
+
+            textBox_BP.Text = BitolaPrimario.ToString();
+
+            textBox_BS.Text = BitolaSecundario.ToString();
+
+
+
+        }
+
+        private void textBox_FR_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
